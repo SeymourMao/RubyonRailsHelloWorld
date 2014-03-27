439c4d14f591fc07f44b22e2de50dd6b5370b777
Seymour Mao <seymourmao@sina.com>
2014-03-27 21:31:34 +0800

    Upload local project to github
    Signed-off-by: Seymour Mao <seymourmao@sina.com>

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+  	puts "I am home"
+  end
+
+  def help
+  	puts "I am help"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'b7cde556ff7f01175ec7af69778ddfa1c98649ae6cc7715e80010de7ae563c949762b4186bbea25f764dd3c14338757facb7673acb950ef2fabc12a7470addd8'
